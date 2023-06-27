14f152c51a9a74b681392981e082707d889a6ec7
kkkkkliu <2293036735@qq.com>
2023-06-28 00:47:18 +0800

    main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 int main(){
    algorithm::Sort sort_instance = algorithm::Sort();
-   std::vector<int> test_vec = {6,5,4,8,222,0,-1,999, 333};
+   std::vector<int> test_vec = {6,5,4,8,3334,0,-1,999, 333};
    sort_instance.BubbleSort(test_vec);
    system("Pause");
    return 0;
