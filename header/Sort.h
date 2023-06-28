604f83604a8c3b6e0390e55b0a874caa7d670c8b
kkkkkliu <2293036735@qq.com>
2023-06-28 21:08:02 +0800

    MergeSort

--- header/Sort.h
+++ header/Sort.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <vector>
 #include "iostream"
+#include "algorithm"
 
 namespace algorithm{
     class Sort
@@ -19,6 +20,7 @@ namespace algorithm{
 
         template <typename T>
         void Print(T& nums){
+            std::cout<<"----------------------------------------------"<<std::endl;
             int length = nums.size();
             for (size_t i = 0; i < length; i++)
             {
@@ -72,6 +74,91 @@ namespace algorithm{
                 swap(nums[left], nums[min_num_index]);
                 left += 1;
             }
+            Print(nums);
+        }
+
+        template <typename T>
+        void InsertSort(T& nums){
+            int length = nums.size();
+            int left = 1;
+            int right = length - 1;
+            while(left <= right){
+                int ptr = left;
+                while (ptr > 0)
+                {
+                   if (nums[ptr] < nums[ptr-1]){
+                    swap(nums[ptr], nums[ptr-1]);
+                    ptr -= 1;
+                   }
+                   else{
+                    break;
+                   }
+                }
+                left += 1;
+            }
+            Print(nums);
+        }
+
+
+        template <typename T>
+        void MergeSortInternal(T& nums, int left, int middle, int right, T& vec){
+            int ptr_left = left;
+            int ptr_right = middle;
+            int index = left;
+            while (ptr_left<middle || ptr_right<right)
+            {
+                if (ptr_left>=middle){
+                    vec[index] = nums[ptr_right++];
+                }
+                else if (ptr_right>=right)
+                {
+                    vec[index] = nums[ptr_left++];
+                }else{
+                    vec[index] = nums[ptr_left]<nums[ptr_right]?nums[ptr_left++]:nums[ptr_right++];
+                }
+                ++index;
+            }
+
+            for (int i = left; i < right; i++)
+            {
+                nums[i] = vec[i];
+            }
+            
+            
+        }
+
+        template <typename T>
+        void MergeSort(T& nums){
+            T vec(nums.size());
+            int length = nums.size();
+            int ceil_len = 1;
+            while(ceil_len < length){
+                for (int i = 0; i<length, i+ceil_len<length; i+=ceil_len*2)
+                {
+                    int left = i;
+                    int middle = i + ceil_len;
+                    int right = std::min(middle + ceil_len, length);
+                    MergeSortInternal(nums, left, middle, right, vec);
+                }
+                ceil_len *= 2;
+            }
+            Print(nums);
+        }
+
+        template <typename T>
+        void MergeSortRecursionInternal(T& nums, int left, int right, T& vec){
+            if (left+1 >= right){
+                return;
+            }
+            MergeSortRecursionInternal(nums, left, (left+right)/2, vec);
+            MergeSortRecursionInternal(nums, (left+right)/2, right, vec);
+            MergeSortInternal(nums, left, (left+right)/2, right, vec);
+        }
+
+        template <typename T>
+        void MergeSortRecursion(T& nums){
+            T vec(nums.size());
+            MergeSortRecursionInternal(nums, 0, nums.size(), vec);
             Print(nums);
         }
 
--- main.cpp
+++ main.cpp
@@ -3,10 +3,15 @@
 #include <vector>
 int main(){
    algorithm::Sort sort_instance = algorithm::Sort();
-   std::vector<double> test_vec = sort_instance.RandVec(10);
-   sort_instance.BubbleSort(test_vec);
-   test_vec = sort_instance.RandVec(10);
-   sort_instance.SelectSort(test_vec);
+   std::vector<double> test_vec = sort_instance.RandVec(100);
+   // sort_instance.BubbleSort(test_vec);
+   // test_vec = sort_instance.RandVec(10);
+   // sort_instance.SelectSort(test_vec);
+   // test_vec = sort_instance.RandVec(10);
+   // sort_instance.InsertSort(test_vec);
+   // test_vec = sort_instance.RandVec(10);
+   // sort_instance.MergeSort(test_vec);
+   sort_instance.MergeSortRecursion(test_vec);
    system("Pause");
    return 0;
 }
